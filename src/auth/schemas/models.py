b4a1d658184f3655c5576292e4f85f093b50759d
masha <mashaovchinnikova01@gmail.com>
2024-11-11 20:46:18 +0500

    feature(auth): Add user caching using redis

--- src/auth/schemas/models.py
+++ src/auth/schemas/models.py
@@ -3,6 +3,8 @@ from typing import List
 from sqlalchemy import Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.auth.schemas.client import ClientSchema
+from src.auth.schemas.operator import OperatorSchema
 from src.number.schemas.models import PhoneNumber
 from src.database import Base
 
@@ -14,6 +16,15 @@ class Operator(Base):
     surname: Mapped[str] = mapped_column(Text, unique=False)
     email: Mapped[str] = mapped_column(Text)
     hashed_password: Mapped[str] = mapped_column(Text)
+
+    def create_schema(self):
+        return OperatorSchema(
+            id=self.id,
+            name=self.name,
+            surname=self.surname,
+            email=self.email,
+            role="operator"
+        )
 
 
 class Client(Base):
@@ -27,5 +38,13 @@ class Client(Base):
     hashed_password: Mapped[str] = mapped_column(Text)
     numbers: Mapped[List["PhoneNumber"]] = relationship()
 
-
+    def create_schema(self, number: str):
+        return ClientSchema(
+            id=self.id,
+            name=self.name,
+            surname=self.surname,
+            patronymic=self.patronymic,
+            login_number=number,
+            role="client"
+        )
 
--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+import redis
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from src.settings import settings
@@ -16,6 +17,12 @@ class Database:
 
 
 database = Database(url=settings.db_url.unicode_string())
-
+redis_db = redis.Redis(
+    host=settings.redis_host,
+    port=settings.redis_port,
+    username=settings.redis_username,
+    password=settings.redis_password,
+    decode_responses=True
+)
 Base = declarative_base()
 
